b465cca825a85b8b7da4adabe2cb779d87f19945
Naison Mutongoreya <naisssmutongoreya@gmail.com>
2026-02-19 02:21:34 +0200

    week_7_assignments

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+    private const double LapDistanceMiles = 50.0 / 1000 * 0.62;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * LapDistanceMiles;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
